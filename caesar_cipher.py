6ce26c0c2480d0d267c91ec8351fe7bef06a0d35
azureguy7 <azureguy7@gmail.com>
2021-08-23 13:54:19 +0900

    feat: Create caesar cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,22 @@
+#Caesar Cipher
+
+- 고전 암호 중에서 가장 간단하고 가장 널리 알려진 하나의 암호화 기법으로 단순 치환 암호
+- Julius Caesar가 개인 서신에서 사용했다고 알려짐
+- 카이사르 암호, 시저 암호, 시프트 암호, 시저의 코드, 서저 소프트라고도 불림.
+
+## 시저 암호화 방법
+- 말고자 하는 알파벳의 고유 순서에서 일정한 간격으로 떨어진 순서의 알파벳을 암호로 표기해주는 방식
+	- 일반문: abcdefghijklmnopqrstuvwxyz
+	- 암호문: wxyzabcdefghijklmnopqrstuv
+	i.e.
+	- 일반문: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+	- 암호문: QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD
+
+## 시저 암호의 효과
+- 시저 암호를 사용할 당시의 효과성은 알려져 있지 않음.
+	- 다만, 시저의 적 대부분이 문맹이고, 외국어라고 오인했을 가능성이 있기 때문에 합리적인 수준에서 안전했을 거라 추정
+	- 당시에는 단순 치환 암호를 푸는 기술에 대한 기록은 없음
+	
+## 시저 암호 해독
+- 열심히 하면 되지 않겠습니까???
+- 지속 보완하겠습니다!!!
